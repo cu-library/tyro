c12b0f47f3af04d320cb0711668795fcb10ad549
Kevin Bowrin <kevin.bowrin@carleton.ca>
2014-09-18 19:36:51 +0000

    Added simple javascript example

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
     "net/url"
     "net/http/httputil"
     "net"
+    "strings"
 )
 
 //The prefix for all the curator environment variables
@@ -28,8 +29,9 @@ const DefaultVerbose bool = false
 //API URL
 const DefaultUrl string = "https://sandbox.iii.com/iii/sierra-api/v1/"
 
-//API Endpoint - Token Request
+//API Endpoints
 const TokenRequestEndpoint string = "token"
+const ItemRequestEndpoint string = "items"
 
 var (
     address = flag.String("address", DefaultAddress, "Address for the server to bind on.")
@@ -49,7 +51,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
     select {
         case token = <-tokenChan:
             logIfVerbose("Received token from internal source.")
-        case <-time.After(5 * time.Second):
+        case <-time.After(20 * time.Second):
             logIfVerbose("Token Error")
     }
 
@@ -58,9 +60,130 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
       return
     }
 
-    fmt.Fprint(w, "<h1>Welcome to Tyro! The Sierra API helper.</h1><div>Current token:</div>")
+    w.Header().Set("Content-Type", "text/html")
 
-    fmt.Fprintf(w, "<pre>%v</pre>", token)
+    fmt.Fprint(w, "<html><h1>Welcome to Tyro! The Sierra API helper.</h1><div>Current token:</div>")
+
+    fmt.Fprintf(w, "<pre>%v</pre></html>", token)
+
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+
+    token := ""
+ 
+    select {
+        case token = <-tokenChan:
+            logIfVerbose("Received token from internal source.")
+        case <-time.After(20 * time.Second):
+            logIfVerbose("Token Error")
+    }
+
+    if token == "" {
+      http.Error(w, "Token Error.", http.StatusInternalServerError)
+      return
+    }
+
+
+    bibID := strings.Split(r.URL.Path[len("/status/"):], "/")[0]
+
+    if bibID == "" {
+        w.Header().Set("Content-Type", "text/html")
+        fmt.Fprint(w, "<html><pre>No bibID set.</pre></html>")
+        return
+    }
+
+    parsedApiUrl, err := url.Parse(*apiUrl)
+    if err != nil {
+        //No recovery possible here, probable problem with URL
+        log.Fatal(err)
+    }
+
+    itemStatusURL := parsedApiUrl
+    itemStatusURL.Path = path.Join(itemStatusURL.Path, ItemRequestEndpoint)
+
+    q := itemStatusURL.Query()
+    q.Set("bibIds", bibID)
+    q.Set("deleted", "false")
+    itemStatusURL.RawQuery = q.Encode()
+
+    getItemStatus, err := http.NewRequest("GET", itemStatusURL.String(), nil)
+    if err != nil {
+        //No recovery possible here, probable problem with URL
+        log.Fatal(err)
+        return
+    }
+
+    getItemStatus.Header.Add("Authorization", "Bearer " + token)
+    getItemStatus.Header.Add("User-Agent", "Tyro")
+
+    originalForwardFor := r.Header.Get("X-Forwarded-For")
+    if originalForwardFor == ""{
+        ip,_,_ := net.SplitHostPort(r.RemoteAddr)
+        getItemStatus.Header.Add("X-Forwarded-For", ip)
+    } else {
+        getItemStatus.Header.Add("X-Forwarded-For", originalForwardFor)
+    }
+
+    client := &http.Client{}
+    resp, err := client.Do(getItemStatus)
+    if err != nil {
+        logIfVerbose(resp)
+        return
+    }
+
+    var responseJson struct {
+        Entries [] struct {
+            CallNumber string `json:"callNumber"`            
+            Status struct {
+                DueDate time.Time `json:"duedate"`
+            } `json:"status"`
+            Location struct {
+                Name string `json:"name"`
+            } `json:"location"` 
+        } `json:"entries"`
+    }
+            
+    err = json.NewDecoder(resp.Body).Decode(&responseJson)
+    defer resp.Body.Close()
+    if err != nil {
+      http.Error(w, "JSON Error", http.StatusInternalServerError)
+      return
+    }
+
+    type Entry struct {
+        CallNumber string 
+        Status string
+        Location string
+    }
+
+    var statusJson struct {
+        Entries []Entry
+    }
+
+    for _,responseEntry := range responseJson.Entries {
+        newEntry := Entry{}
+        newEntry.CallNumber = responseEntry.CallNumber
+        newEntry.CallNumber = strings.Replace(newEntry.CallNumber, "|a", " ", -1)
+        newEntry.CallNumber = strings.Replace(newEntry.CallNumber, "|b", " ", -1)
+        if  responseEntry.Status.DueDate.IsZero() {
+            newEntry.Status = "IN LIBRARY"
+        } else {
+            newEntry.Status = "DUE " + responseEntry.Status.DueDate.Format("January 2, 2006")
+        }
+        newEntry.Location = responseEntry.Location.Name
+
+        statusJson.Entries = append(statusJson.Entries, newEntry)
+    }
+
+    json, err := json.MarshalIndent(statusJson, "", "   ")
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+  
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(json)
 
 }
 
@@ -71,7 +194,7 @@ func rawRewriter(r *http.Request) {
     select {
         case token = <-tokenChan:
             logIfVerbose("Received token from internal source.")
-        case <-time.After(5 * time.Second):
+        case <-time.After(20 * time.Second):
             logIfVerbose("Token Error")
     }
 
@@ -79,17 +202,16 @@ func rawRewriter(r *http.Request) {
     if err != nil {
         //No recovery possible here, probable problem with URL
         log.Fatal(err)
-        return
     }
 
-    parsedApiUrl.Path = path.Join(parsedApiUrl.Path, r.URL.Path[len("/raw/"):])
-    parsedApiUrl.RawQuery = r.URL.RawQuery
+    rawRequestURL := parsedApiUrl
+    rawRequestURL.Path = path.Join(rawRequestURL.Path, r.URL.Path[len("/raw/"):])
+    rawRequestURL.RawQuery = r.URL.RawQuery
 
-    r.URL = parsedApiUrl
+    r.URL = rawRequestURL
     
     r.Header.Add("Authorization", "Bearer " + token)
     r.Header.Add("User-Agent", "Tyro")
-    r.Header.Add("Content-Type", "text/html")
 
     originalForwardFor := r.Header.Get("X-Forwarded-For")
     if originalForwardFor == ""{
@@ -112,7 +234,7 @@ func tokener(){
         ExpiresIn int `json:"expires_in"`
     }
 
-    token := ""
+    token := "uninitialized"
 
     for {
         select {
@@ -140,15 +262,15 @@ func tokener(){
                 if err != nil {
                     //No recovery possible here, probable problem with URL
                     log.Fatal(err)
-                    return
                 }
 
-                parsedApiUrl.Path = path.Join(parsedApiUrl.Path, TokenRequestEndpoint)
+                tokenRequestURL := parsedApiUrl
+                tokenRequestURL.Path = path.Join(tokenRequestURL.Path, TokenRequestEndpoint)
                 
                 bodyValues := url.Values{}
                 bodyValues.Set("grant_type", "client_credentials")
 
-                getTokenRequest, err := http.NewRequest("POST", parsedApiUrl.String(), bytes.NewBufferString(bodyValues.Encode()))
+                getTokenRequest, err := http.NewRequest("POST", tokenRequestURL.String(), bytes.NewBufferString(bodyValues.Encode()))
                 if err != nil {
                     //No recovery possible here, probable problem with URL
                     log.Fatal(err)
@@ -224,9 +346,11 @@ func main() {
     refreshTokenChan <- true
 
     http.HandleFunc("/", homeHandler)
+    http.HandleFunc("/status/", statusHandler)
     rawProxy := httputil.NewSingleHostReverseProxy(&url.URL{})
     rawProxy.Director = rawRewriter
     http.Handle("/raw/", rawProxy)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
     err := http.ListenAndServe(*address, nil)
     if err != nil {
         log.Fatal(err)
